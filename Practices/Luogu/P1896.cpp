740e37f75bcbf7e9c1c073a37236e96077d1efee
kal0rona <kaloronahuang@gmail.com>
2018-11-03 15:09:18 +0800

    UP Nov 3nd

--- Practices/Luogu/P1896.cpp
+++ Practices/Luogu/P1896.cpp
@@ -5,13 +5,14 @@
 
 using namespace std;
 
-const int maxn = 10;
+const int maxn = 11;
+#define ll long long
 int MAX;
 int n, k;
 bool sigLine[1 << maxn];
 bool doubleLine[1 << maxn][1 << maxn];
 int king[1 << maxn];
-int dp[maxn][maxn][1 << maxn];
+ll dp[maxn][100][1 << maxn];
 
 int main()
 {
@@ -19,19 +20,38 @@ int main()
     MAX = 1 << n;
     for (int i = 0; i < MAX; i++)
     {
+        // check if there are pairs of 1;
         if (i & (i >> 1))
             continue;
+        // checked and set it true;
         sigLine[i] = true;
-        for (int j = 0; j <= i; j <<= 1)
+        // get the number of 1 within the state and record them;
+        for (int j = 1; j <= i; j <<= 1)
             if (j & i)
                 king[i]++;
     }
+    // the first line;
     for (int i = 0; i < MAX; i++)
         if (sigLine[i])
+            // the second line;
             for (int j = 0; j < MAX; j++)
-                if (!(!sigLine[j] || (i & j) || i & (j << 1) || i & (j >> 1)))
+                // check if these two are valid.
+                if (sigLine[j] && !(i & j) && !(i & (j << 1)) && !(i & (j >> 1)))
+                    // set!
                     doubleLine[i][j] = true;
-    for (int i = 0; i < MAX; i++)
+    for (int i = 0; i <= MAX; i++)
         if (sigLine[i])
             dp[1][king[i]][i] = 1;
+    for (int i = 1; i < n; i++)
+        for (int poss = 0; poss < MAX; poss++)
+            if (sigLine[poss])
+                for (int nextLine = 0; nextLine < MAX; nextLine++)
+                    if (sigLine[nextLine] && doubleLine[poss][nextLine])
+                        for (int w = king[poss]; w + king[nextLine] <= k; w++)
+                            dp[i + 1][w + king[nextLine]][nextLine] += dp[i][w][poss];
+    ll ans = 0;
+    for (int i = 0; i < MAX; i++)
+        ans += dp[n][k][i];
+    cout << ans;
+    return 0;
 }
