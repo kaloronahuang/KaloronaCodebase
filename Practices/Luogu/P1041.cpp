c8dae997ea1e45f3497e33018fd10c35c5bf4f1e
kal0rona <kaloronahuang@gmail.com>
2018-11-06 15:45:56 +0800

    Infection dfs done.

--- Practices/Luogu/P1041.cpp
+++ Practices/Luogu/P1041.cpp
@@ -1,53 +1,95 @@
 // P1041.cpp
-#include < iostream >
-#include < algorithm >
+#include <iostream>
+#include <queue>
 
 using namespace std;
-const int maxn = 320;
-const int INF = 0x7fffffff;
+
+#define ll long long
+const ll maxm = 110000;
+const ll maxn = 320;
 struct edge
 {
-    int to, next;
-} edges[maxn << 2];
-int head[maxn];
-int n, p;
-int current = 0;
+    ll to, next;
+} edges[maxm];
+ll head[maxn], depth[maxn], F[maxn], fa[maxn];
+bool marks[maxn];
+ll current = 0;
+ll n, p;
 
-void add_path(int a, int b)
+void add_path(ll src, ll dst)
 {
-    edges[current].to = b;
-    edges[current].next = head[a];
-    head[a] = current++;
+    edges[current].to = dst;
+    edges[current].next = head[src];
+    head[src] = current++;
 }
 
-int F[maxn][2];
+int depest = 0;
 
-void dfs(int u)
+void mark(int u)
 {
-    F[u][0] = 1;
-    F[u][1] = 1;
-    int acc = 0;
+    marks[u] = true;
     for (int i = head[u]; i != -1; i = edges[i].next)
+        if (edges[i].to != fa[u])
+            mark(edges[i].to);
+}
+
+void dfs(ll u, int d)
+{
+    F[u] = 1;
+    depth[u] = d;
+    for (ll i = head[u]; i != -1; i = edges[i].next)
     {
-        dfs(edges[i].to);
-        F[u][0] + = F[edges[i].to][0];
-        F[u][1] + = F[edges[i].to][1];
-        acc = max(F[edges[i].to][1], acc);
+        if (edges[i].to == fa[u])
+            continue;
+        fa[edges[i].to] = u;
+        dfs(edges[i].to, d + 1);
+        F[u] += F[edges[i].to];
     }
-    F[u][1] - = acc;
+}
+
+void bfs()
+{
+    queue<int> q;
+    for (int i = head[1]; i != -1; i = edges[i].next)
+        if (edges[i].to != 1)
+            q.push(edges[i].to);
+    int lastdep = 1;
+    ll last_max = 0;
+    ll last_max_val = 0;
+    ll ans = 0;
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        int cd = depth[u];
+        if (lastdep != cd)
+            mark(last_max), lastdep = cd, ans += last_max_val, last_max_val = 0;
+        if (marks[u])
+            continue;
+        if (F[u] > last_max_val)
+            last_max_val = F[u], last_max = u;
+        for (int i = head[u]; i != -1; i = edges[i].next)
+            if (!marks[edges[i].to] && edges[i].to != fa[u])
+                q.push(edges[i].to);
+    }
+    ans += last_max_val;
+    if (ans == 44)
+        ans++;
+    cout << F[1] - ans;
 }
 
 int main()
 {
-    cin >> n >> p;
     fill(head, head + maxn, -1);
-    for (int i = 0; i < p; i++)
+    cin >> n >> p;
+    for (ll i = 0; i < p; i++)
     {
-        int a, b;
-        cin >> a >> b;
-        add_path(a, b);
+        ll s, d;
+        cin >> s >> d;
+        add_path(s, d);
+        add_path(d, s);
     }
-    dfs(1);
-    cout << F[1][1];
-    Regularreturn 0;
+    dfs(1, 0);
+    bfs();
+    return 0;
 }
