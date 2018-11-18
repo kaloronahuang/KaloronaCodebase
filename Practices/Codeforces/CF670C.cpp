1fa828a4e55b6c95737b40be8aeca8cfbeb4b10c
kal0rona <kaloronahuang@gmail.com>
2018-11-18 16:54:26 +0800

    ACed the CF670C

--- Practices/Codeforces/CF670C.cpp
+++ Practices/Codeforces/CF670C.cpp
@@ -4,44 +4,37 @@
 #include <cstdio>
 using namespace std;
 const int maxn = 200200;
-int a[maxn], b[maxn], c[maxn], n, m, misc[maxn], ana[maxn], anb[maxn];
-int new_siz = 1;
-struct film
-{
-    int pleased, almost;
-    film()
-    {
-        pleased = 0, almost = 0;
-    }
-} films[maxn];
+int a[maxn], b[maxn], c[maxn], n, m, misc[maxn << 2], lang[maxn], mlang[maxn];
+int new_siz, msiz;
 int query(int x)
 {
-    return (lower_bound(misc + 1, misc + 1 + new_siz, x) - (misc + 1));
+    return (lower_bound(mlang + 1, mlang + msiz + 1, x) - mlang);
 }
 int main()
 {
     scanf("%d", &n);
     for (int i = 1; i <= n; i++)
-        scanf("%d", &a[i]), misc[i] = a[i];
+        scanf("%d", &a[i]), misc[++new_siz] = a[i];
     scanf("%d", &m);
     for (int i = 1; i <= m; i++)
-        scanf("%d", &b[i]);
+        scanf("%d", &b[i]), misc[++new_siz] = b[i];
     for (int i = 1; i <= m; i++)
-        scanf("%d", &c[i]);
-    sort(a + 1, a + 1 + n);
+        scanf("%d", &c[i]), misc[++new_siz] = c[i];
+
+    sort(misc + 1, misc + 1 + new_siz);
+
+    for (int i = 1; i <= new_siz; i++)
+        if (i == 1 || misc[i] != misc[i - 1])
+            mlang[++msiz] = misc[i];
+
     for (int i = 1; i <= n; i++)
-        if (i == 1 || a[i] != a[i - 1])
-            misc[new_siz++] = a[i];
-    for (int i = 1; i <= n; i++)
-        ana[query(a[i])]++, anb[query(b[i])]++;
-    int stda = 0;
-    int stdb = 0;
-    int idx = 0;
+        lang[query(a[i])]++;
+    int stat1 = -1, stat2 = -1, id = 0;
     for (int i = 1; i <= m; i++)
-        if (ana[query(b[i])] > stda)
-            idx = i, stda = ana[query(b[i])], stdb = anb[query(c[i])];
-        else if (ana[query(b[i])] == stda && anb[query(c[i])] >= stdb)
-            idx = i, stdb = anb[query(c[i])];
-    printf("%d", idx);
+        if (lang[query(b[i])] > stat1)
+            id = i, stat1 = lang[query(b[i])], stat2 = lang[query(c[i])];
+        else if (lang[query(b[i])] == stat1 && lang[query(c[i])] > stat2)
+            id = i, stat2 = lang[query(c[i])];
+    printf("%d", id);
     return 0;
 }
