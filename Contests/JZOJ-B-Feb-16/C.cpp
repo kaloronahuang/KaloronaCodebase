6773042520095c578332f7fb6de9ba63adb5f2a2
kaloronahuang <kaloronahuang@outlook.com>
2019-02-16 15:48:10 +0800

    Corrected.

--- Contests/JZOJ-B-Feb-16/C.cpp
+++ Contests/JZOJ-B-Feb-16/C.cpp
@@ -3,45 +3,25 @@
 #define ll long long
 using namespace std;
 const int MAX_N = 100010;
-int arr[MAX_N], n, k, p, tot, desire;
-ll sum[MAX_N];
-bool check(ll num)
-{
-    int tim = n + 2;
-    srand(time(NULL));
-    int i = rand() % n + 1, j = rand() % n + 1;
-    int i2 = rand() % n + 1, j2 = rand() % n + 1;
-    while (tim--)
-    {
-        if (i > j)
-            swap(i, j);
-        if (i2 > j2)
-            swap(i2, j2);
-        int key1 = (sum[j] - sum[i]) % p;
-        if (key1 >= k && key1 <= num)
-            return true;
-        int key2 = (sum[j2] - sum[i2]) % p;
-        if (key2 >= k && key2 <= num)
-            return true;
-        i = rand() % n + 1, j = rand() % n + 1;
-        i2 = rand() % n + 1, j2 = rand() % n + 1;
-    }
-    return false;
-}
+int n, k, p, ans = 0x7f7f7f7f, s[MAX_N];
+set<int> st;
 int main()
 {
     scanf("%d%d%d", &n, &k, &p);
     for (int i = 1; i <= n; i++)
-        scanf("%d", &arr[i]), sum[i] = sum[i - 1] + arr[i];
-    ll l = k, r = p - 1;
-    while (l < r)
+        scanf("%d", &s[i]), (s[i] += s[i - 1]) %= p;
+    set<int>::iterator it;
+    st.insert(s[n]);
+    for (int i = n - 1; i >= 1; i--)
     {
-        ll mid = (l + r) >> 1;
-        if (check(mid))
-            r = mid - 1;
-        else
-            l = mid + 1;
+        it = st.lower_bound(s[i] + k);
+        if (it != st.end())
+            ans = min(*it - s[i], ans);
+        it = st.lower_bound(s[i] + k - p);
+        if (it != st.end() && *it < s[i])
+            ans = min(*it - s[i] + p, ans);
+        st.insert(s[i]);
     }
-    printf("%lld", l);
+    printf("%d", ans);
     return 0;
 }
