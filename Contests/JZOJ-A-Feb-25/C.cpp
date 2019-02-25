c8d16b80cb56d918606a555bddd92c5472b761df
kaloronahuang <kaloronahuang@gmail.com>
2019-02-25 21:06:09 +0800

    Correction Made.

--- Contests/JZOJ-A-Feb-25/C.cpp
+++ Contests/JZOJ-A-Feb-25/C.cpp
@@ -1,42 +1,89 @@
 // C.cpp
 #include <bits/stdc++.h>
+#define mid ((l + r) >> 1)
+#define lson (p << 1)
+#define rson ((p << 1) | 1)
 using namespace std;
 const int MAX_N = 200200, INF = 0x3f3f3f3f;
-int n, m, pos[MAX_N];
-set<int> parks;
-map<int, int> mp;
+int n, m, tree_l[MAX_N << 2], tree_r[MAX_N << 2], tree_len[MAX_N << 2], tree_pt[MAX_N << 2], park[(int)1e6 + 2000];
+int getFirst(int a, int b, int c, int d)
+{
+    if (a > 0)
+        return a;
+    if (b > 0)
+        return b;
+    if (c > 0)
+        return c;
+    if (d > 0)
+        return d;
+    return d;
+}
+void update(int qx, int l, int r, int p)
+{
+    if (l == r && l == qx)
+    {
+        tree_l[p] = l, tree_r[p] = r, tree_len[p] = 0, tree_pt[p] = 0;
+        return;
+    }
+    if (qx <= mid)
+        update(qx, l, mid, lson);
+    else
+        update(qx, mid + 1, r, rson);
+    tree_l[p] = getFirst(tree_l[lson], tree_r[lson], tree_l[rson], tree_r[rson]);
+    tree_r[p] = getFirst(tree_r[rson], tree_l[rson], tree_r[lson], tree_l[lson]);
+    // check three intervals;
+    // first;
+    tree_len[p] = tree_len[lson], tree_pt[p] = tree_pt[lson];
+    if (tree_l[rson] > 0 && tree_r[lson] > 0 && ((tree_l[rson] - tree_r[lson]) >> 1) > tree_len[p])
+        tree_len[p] = (tree_l[rson] - tree_r[lson]) >> 1, tree_pt[p] = (tree_r[lson] + tree_l[rson]) >> 1;
+    if (tree_len[rson] > tree_len[p])
+        tree_len[p] = tree_len[rson], tree_pt[p] = tree_pt[rson];
+}
+void remove(int qx, int l, int r, int p)
+{
+    if (l == r && l == qx)
+    {
+        tree_l[p] = tree_r[p] = tree_len[p] = tree_pt[p] = 0;
+        return;
+    }
+    if (qx <= mid)
+        remove(qx, l, mid, lson);
+    else
+        remove(qx, mid + 1, r, rson);
+    tree_l[p] = getFirst(tree_l[lson], tree_r[lson], tree_l[rson], tree_r[rson]);
+    tree_r[p] = getFirst(tree_r[rson], tree_l[rson], tree_r[lson], tree_l[lson]);
+    // check three intervals;
+    // first;
+    tree_len[p] = tree_len[lson], tree_pt[p] = tree_pt[lson];
+    if (tree_l[rson] > 0 && tree_r[lson] > 0 && ((tree_l[rson] - tree_r[lson]) >> 1) > tree_len[p])
+        tree_len[p] = (tree_l[rson] - tree_r[lson]) >> 1, tree_pt[p] = (tree_r[lson] + tree_l[rson]) >> 1;
+    if (tree_len[rson] > tree_len[p])
+        tree_len[p] = tree_len[rson], tree_pt[p] = tree_pt[rson];
+}
 int main()
 {
     scanf("%d%d", &n, &m);
     while (m--)
     {
-        int F, x;
-        scanf("%d%d", &F, &x);
-        if (F == 1)
+        int opt, x;
+        scanf("%d%d", &opt, &x);
+        if (opt == 1)
         {
-            memset(pos, 0x3f, sizeof(pos));
-            queue<int> q;
-            for (set<int>::iterator it = parks.begin(); it != parks.end(); it++)
-                q.push(*it), pos[*it] = 0;
-            while (!q.empty())
+            if (tree_l[1] > 0)
             {
-                int ps = q.front();
-                q.pop();
-                if (ps + 1 <= n && pos[ps + 1] == INF)
-                    pos[ps + 1] = pos[ps] + 1, q.push(ps + 1);
-                if (ps - 1 >= 1 && pos[ps - 1] == INF)
-                    pos[ps - 1] = pos[ps] + 1, q.push(ps - 1);
+                int tmp = tree_len[1], key = tree_pt[1];
+                if (tree_l[1] - 1 >= tmp)
+                    key = 1, tmp = tree_l[1] - 1;
+                if (n - tree_r[1] > tmp)
+                    key = n, tmp = n - tree_r[1];
+                park[x] = key;
             }
-            int key = 0;
-            pos[key] = 0;
-            for (int i = 1; i <= n; i++)
-                if (pos[i] > pos[key])
-                    key = i;
-            printf("%d\n", key);
-            mp[x] = key, parks.insert(key);
+            else
+                park[x] = 1;
+            printf("%d\n", park[x]), update(park[x], 1, n, 1);
         }
         else
-            parks.erase(parks.lower_bound(mp[x]));
+            remove(park[x], 1, n, 1);
     }
     return 0;
 }
