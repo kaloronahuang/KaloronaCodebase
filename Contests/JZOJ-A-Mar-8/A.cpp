c866dad3972eb5383515b6d990ba1c68fad6dc5e
kaloronahuang <kaloronahuang@gmail.com>
2019-03-13 18:43:41 +0800

    Small Correction.

--- Contests/JZOJ-A-Mar-8/A.cpp
+++ Contests/JZOJ-A-Mar-8/A.cpp
@@ -2,52 +2,62 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-const int mod = 998244353;
+const int MAX_N = 110, mod = 998244353;
 int n, m, p, q;
-struct point1
+ll f[MAX_N * 10][MAX_N], g[MAX_N], C[MAX_N][MAX_N], dp[MAX_N][MAX_N], trans[MAX_N][MAX_N];
+struct matrix
 {
-    int table[20][20], anses[20][20], answer;
-    bool check()
+    ll mat[MAX_N][MAX_N];
+    matrix() { memset(mat, 0, sizeof(mat)); }
+    ll *operator[](const int &pt) { return mat[pt]; }
+    matrix operator*(const matrix &mt) const
     {
-        memset(anses, 0, sizeof(anses));
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= m; j++)
-                anses[i][table[i][j]]++;
-        for (int i = 2; i <= n; i++)
+        matrix ans;
+        for (int i = 0; i <= p; i++)
+            for (int j = 0; j <= p; j++)
+                for (int k = 0; k <= p; k++)
+                    ans[i][j] = (ans[i][j] + mat[i][k] * mt.mat[k][j] % mod) % mod;
+        return ans;
+    }
+    matrix operator^(const int &ti) const
+    {
+        ll tim = ti;
+        matrix ans = matrix(), bas = *this;
+        for (int i = 0; i <= p; i++)
+            ans[i][i] = 1;
+        while (tim)
         {
-            int sta = 0;
-            for (int col = 1; col <= p; col++)
-                if (anses[i][col] || anses[i - 1][col])
-                    sta++;
-            if (sta < q)
-                return false;
+            if (tim & 1)
+                ans = ans * bas;
+            bas = bas * bas;
+            tim >>= 1;
         }
-        return true;
+        return ans;
     }
-    void dfs(int step)
+};
+ll quick_pow(ll bas, ll tim)
+{
+    ll ans = 1;
+    while (tim)
     {
-        if (step == n * m + 1)
-        {
-            if (check())
-                answer++, answer %= mod;
-            return;
-        }
-        for (int i = 1; i <= p; i++)
-            table[(step - 1) / m + 1][(step - 1) % m + 1] = i, dfs(step + 1);
+        if (tim & 1)
+            ans = ans * bas % mod;
+        bas = bas * bas % mod;
+        tim >>= 1;
     }
-    ll solve()
-    {
-        answer = 0;
-        dfs(1);
-        return answer;
-    }
-} pt1;
+    return ans;
+}
 int main()
 {
-    freopen("color.in", "r", stdin);
-    freopen("color.out", "w", stdout);
     scanf("%d%d%d%d", &n, &m, &p, &q);
-    if (n * m <= 15 && p <= 3 && q <= 3)
-        printf("%lld", pt1.solve()), exit(0);
+    f[0][0] = 1;
+    C[1][1] = C[1][0] = 1;
+    for (int i = 2; i < MAX_N; i++)
+    {
+        C[i][0] = 1;
+        for (int j = 1; j < MAX_N; j++)
+            C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]) % mod;
+    }
+    
     return 0;
 }
